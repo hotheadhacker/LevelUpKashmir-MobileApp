52b58ff0611a553951ddb8ff6ce039b5ae076397
hotheadhacker <isalmanqureshi@gmail.com>
2020-12-05 00:58:40 +0530

    Updates And Fixes

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,21 +1,62 @@
 import React, {useState} from 'react';
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, Image } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Home(){
-    const [news, setNews] = useState([
-        { text: 'buy coffee', key: '1' },
-        { text: 'create an app', key: '2' },
-        { text: 'play on the switch', key: '3' }
-      ]);
-    
+    const [news, setNews] = useState(0);
+
+    //expo fonts
+    let [fontsLoaded] = useFonts({
+        'Nexa-Bold': require('../../assets/fonts/Nexa-Bold.otf'),
+      });
+    //Get articles first try using async - await
+    const getLatestArticles = async () => {
+        try {
+          let response = await fetch(
+            'https://levelupkashmir.com/wp-json/wp/v2/posts?_embed'
+          );
+          let json = await response.json();
+        //   return json.movies;
+        setNews(json);
+        // console.log(json);
+        } catch (error) {
+        //   console.error(error);
+        }
+      }; 
+      //call get function
+      getLatestArticles();    
+    //   function to convert gmt to ist
+    function gmtToIst(date){
+      var s = new Date(date).toLocaleString(undefined, {timeZone: 'Asia/Kolkata'});
+      return s
+    }
     return(
         <View style={styles.container}>
+            {/* <Text></Text>
             <Text style={styles.fontHeader}>Hello From the other side(Home)</Text>
-    <Text style={styles.fontHeader}>Hello From the other side(Home){news[0].text}</Text>
+    <Text style={styles.fontHeader}>Hello From the other side(Home)</Text> */}
             <FlatList
+            keyExtractor={item => item.id}
             data={news}
             renderItem={({ item }) => (
-              <Text>{item.text}</Text>
+              
+                <View style={styles.card}>
+                    
+                <Image source = {{uri: item._embedded["wp:featuredmedia"][0].source_url}}
+   style = {styles.image}
+   />
+            
+              
+            <Text style={styles.title}>{item.title.rendered}{'\n'}<Text style={styles.lable}>
+            <Entypo name="calendar" size={12} color="black" /> {gmtToIst(item.date_gmt)}</Text> 
+            <Text style={styles.tag}> <AntDesign name="tags" size={12} color="black" /> 
+            {item._embedded["wp:term"][0][0].name}
+            </Text>  
+            </Text>
+            
+              </View>
             )}
           />
         </View>
@@ -24,7 +65,7 @@ export default function Home(){
 const styles = StyleSheet.create({
     container: {
       // flex: 1,
-      backgroundColor: '#bbb' ,
+      backgroundColor: '#dffafa' ,
       alignItems: 'center',
       justifyContent: 'center',
       
@@ -34,9 +75,48 @@ const styles = StyleSheet.create({
     //   fontWeight: 'bold',
       fontSize: 20,
       color: 'white',
-      
-      
-
-
-    }
+    },
+     title: {
+    //  textAlign: 'center',
+     fontFamily: 'Nexa-Bold',
+     fontSize: 20,
+     width: 320,
+     marginTop: 10,
+     marginBottom: 10,
+    },
+    card: {
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: 'white',
+        marginTop: 10,
+        borderWidth: 4,
+        borderColor: "#3f9e9e",
+        borderRadius: 6,
+    //     alignItems: 'center',
+    //   justifyContent: 'center',
+        
+    },
+    image: {
+        width: 50,
+        height: 50,
+        marginTop: 10,
+        marginBottom: 10,
+        marginRight: 5,
+        marginLeft: 5,
+    },
+    lable:{
+        backgroundColor: '#d4dae3',
+        fontSize: 10,
+        borderWidth: 4,
+        borderRadius: 20,
+        fontFamily: 'sans-serif'
+    },
+    tag:{
+        backgroundColor: '#a4f64d',
+        fontSize: 10,
+        borderWidth: 4,
+        borderRadius: 20,
+        fontFamily: 'sans-serif',
+        marginLeft: 5
+    },
   });
